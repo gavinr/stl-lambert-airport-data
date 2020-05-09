202e2372195ce1dae93c00661691240f4baadba7
Gavin Rehkemper <gavin@gavinr.com>
2020-05-09 11:25:24 -0500

    build site using github actions

--- site/src/server.js
+++ site/src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'stl-lambert-airport-data',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
