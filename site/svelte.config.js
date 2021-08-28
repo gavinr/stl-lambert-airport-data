dd50279da61e533cc7239237fe8dd958dfaf074f
Gavin Rehkemper <gavin@gavinr.com>
2021-08-27 22:34:30 -0500

    publish

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,9 +7,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
+	paths: {
+		base: '/stl-lambert-airport-data',
+	},
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({})
 	}
 };
 
