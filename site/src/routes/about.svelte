cf2ace27dda727ea8253323e60cd23d118fd30de
Gavin Rehkemper <gavin@gavinr.com>
2021-12-03 17:16:41 -0600

    link to STL Aviation news

--- site/src/routes/about.svelte
+++ site/src/routes/about.svelte
@@ -1,18 +1,25 @@
 <svelte:head>
-  <title>About</title>
+	<title>About</title>
 </svelte:head>
 
 <h1>About - St. Louis Lambert Airport Statistics</h1>
 
 <p>
-  This site features visualizations of the open data of the
-  <a href="https://github.com/gavinr/stl-lambert-airport-data">
-    St. Louis Lambert International Airport Data
-  </a>
-  . To use the data yourself, or to log an issue or comment about this site,
-  please see the
-  <a href="https://github.com/gavinr/stl-lambert-airport-data">GitHub page</a>.
+	This site features visualizations of the open data of the
+	<a href="https://github.com/gavinr/stl-lambert-airport-data">
+		St. Louis Lambert International Airport Data
+	</a>
+	. To use the data yourself, or to log an issue or comment about this site, please see the
+	<a href="https://github.com/gavinr/stl-lambert-airport-data">GitHub page</a>.
 </p>
 <p>
-  This site built and maintained by <a href="https://gavinr.com">Gavin Rehkemper</a>.
+	For more good St. Louis airport data, go to <a
+		rel="nofollow"
+		target="_blank"
+		href="https://aviationstl.wixsite.com/website">STL Aviation News</a
+	>.
+</p>
+
+<p>
+	This site built and maintained by <a href="https://gavinr.com">Gavin Rehkemper</a>.
 </p>
